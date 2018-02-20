9101a458505459633e65a3edee42713ee185bc14
Khaled <elgendy1204@gmail.com>
2018-02-20 23:01:48 +0200

    finish /new logic

--- app.js
+++ app.js
@@ -24,31 +24,33 @@ app.get('/new/:url', (req, res) => {
     const url = req.params.url;
     callDb(function (db) {
         const collection = db.collection('urls');
-        collection.findOne({
+        const operation = collection.findOne({
             url: {
                 $eq: url
             }
         }).then(data => {
-            if(!data) {
-                collection.insertOne({
+            if (!data) {
+                return collection.insertOne({
                     url: url
                 })
                     .then(function (result) {
                         res.json({
                             original_url: url,
-                            'short_url': `${req.headers.host}/${result.id}`
+                            'short_url': `${req.headers.host}/${result.ops[0]._id}`
                         });
+                        return result;
+                    }).catch(err => {
+                        res.json(err);
                     });
             }
+            res.json(data);
+            return data;
         }).catch(err => {
-            res.json({ err });
+            res.json(err);
+            return err;
         });
+        return operation;
     });
 });
-
-
-// callDb(function(db) {
-//     const collection = db.collection('urls');
-// });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!')); // eslint-disable-line
--- db.js
+++ db.js
@@ -1,11 +1,16 @@
 const MongoClient = require('mongodb').MongoClient;
-const dbUrl = `mongodb://${process.env.dbuser}:${process.env.dbpassword}@${process.env.dburl}`;
+const dbUrl = process.env.env != 'dev' ? `mongodb://${process.env.dbuser}:${process.env.dbpassword}@${process.env.dburl}` :
+    process.env.localdb;
 
-module.exports = function (callback) {
-    MongoClient.connect(dbUrl, function (err, client) {
+module.exports = async function (callback) {
+    await MongoClient.connect(dbUrl, async function (err, client) {
         if (err) return console.log(err); //eslint-disable-line
-        const db = client.db(process.env.dbName);
-        callback(db);
-        client.close();
+        const db = client.db(process.env.dbname);
+        try {
+            await callback(db);
+            client.close();
+        } catch (error) {
+            client.close();
+        }
     });
 };
