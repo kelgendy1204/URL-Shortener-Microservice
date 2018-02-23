c228066848f41ead7a4c7dfc0ba70d8594d629b7
Khaled <elgendy1204@gmail.com>
2018-02-23 22:36:47 +0200

    finish app

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
-require('dotenv').config();
 
+require('dotenv').config();
+const ObjectID = require('mongodb').ObjectID;
 const express = require('express');
 const app = express();
 const callDb = require('./db');
+const validUrl = require('valid-url');
 
 app.get('/:id', (req, res) => {
-    const id = req.params.url;
+    const _id = req.params.id;
     callDb(function(db) {
         const collection = db.collection('urls');
-        collection.findOne({
-            id: {
-                $eq: id
-            }
+        return collection.findOne({
+            _id: new ObjectID(_id)
         }).then(data => {
-            res.json({ data });
+            if (data) {
+                res.writeHead(302, {
+                    'Location': data.url
+                });
+                res.end();
+            }
+            res.end('no such link');
+            return data;
         }).catch(err => {
             res.json({ err });
         });
     });
 });
 
-app.get('/new/:url', (req, res) => {
-    const url = req.params.url;
+app.get('/new/*', (req, res) => {
+    const url = req.params[0];
+    if (!validUrl.isUri(url)){
+        throw 'Not a valid url';
+    }
     callDb(function (db) {
         const collection = db.collection('urls');
         const operation = collection.findOne({
@@ -42,9 +52,13 @@ app.get('/new/:url', (req, res) => {
                     }).catch(err => {
                         res.json(err);
                     });
+            } else {
+                res.json({
+                    original_url: url,
+                    short_url: `${req.headers.host}/${data._id}`
+                });
+                return data;
             }
-            res.json(data);
-            return data;
         }).catch(err => {
             res.json(err);
             return err;
